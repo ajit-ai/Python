8a76fd33454005fd341ff3107eaeee6441f507e3
ajitj <ajitk123@gmail.com>
2024-08-23 22:35:19 +0530

    variable as complex

---
+++ number/complexnumber.py
@@ -0,0 +1,13 @@
+x = 1+7j  #All complex numbers are imaginary as j;
+y = 7j
+z = -7j
+a = -1+7j
+b = 2/3j
+c = 2*3j
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+print(type(b))
+print(type(c))
