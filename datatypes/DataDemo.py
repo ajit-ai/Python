d1a307f19808eb5df26846dd167d9e7ef1c8c2c5
ajitj <ajitk123@gmail.com>
2024-08-23 22:16:40 +0530

    Data types

---
+++ datatypes/DataDemo.py
@@ -0,0 +1,123 @@
+
+#Text Type:	str
+#Numeric Types:	int, float, complex
+#Sequence Types:	list, tuple, range
+#Mapping Type:	dict
+#Set Types:	set, frozenset
+#Boolean Type:	bool
+#Binary Types:	bytes, bytearray, memoryview
+#None Type:	NoneType
+
+
+x = str("India")
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = int(20)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = float(8.7)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = complex(1j)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = list(("A", "B", "C"))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = tuple(("A", "B", "C"))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = range(6)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = dict(name="Ajit", age=36)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = set(("A", "B", "C", "D", "E", "F", "G", "H"))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = frozenset(("A", "B", "C", "D", "E", "F", "G", "H"))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = bool(5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = bytes(5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = bytearray(7)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = memoryview(bytes(4))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
