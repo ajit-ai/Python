2ed2b4ab962eae0d484ad37fefe839aaa0fe07c3
ajitj <ajitk123@gmail.com>
2025-07-11 07:00:19 +0530

    updates

---
+++ basic/operator.py
@@ -0,0 +1,28 @@
+x = 5
+y = 10
+print("1.Addition:", x + y)  # Addition
+print("2.Subtraction:", x - y)  # Subtraction
+print("3.Multiplication", x * y)  # Multiplication
+print("4.Division", x / y)  # Division
+print(x ** y)  # x raised to the power of y
+print(x // y)  # floor division
+print(x % y)   # modulus
+print(x & y)   # bitwise AND
+print(x | y)   # bitwise OR
+print(x ^ y)   # bitwise XOR
+print(x << 1)  # left shift
+print(x >> 1)  # right shift
+print(~x)      # bitwise NOT
+print(x == y)  # equal to
+print(x != y)  # not equal to
+print(x > y)   # greater than
+print(x < y)   # less than
+print(x >= y)  # greater than or equal to
+print(x <= y)  # less than or equal to
+print(x and y)  # logical AND
+print(x or y)   # logical OR
+print(not x)     # logical NOT
+print(x is y)   # identity operator
+print(x is not y)  # identity operator
+#print(x in y)   # membership operator
+#print(x not in y)  # membership operator
