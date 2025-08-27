df88fa750be38940642140b55bed16fe8f0f6ad8
Ajit Kumar <ajitjava2@gmail.com>
2025-08-27 22:42:36 +0530

    python update

---
+++ exp/functioncall.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  8 07:14:54 2025
+
+@author: ajitj
+"""
+# why using def whith def u can call with zero parameter how
+
+def function1():
+    print("Called function1")
+    
+def function2():
+    print("Callled function2")
+    
+function1()
+function2()
